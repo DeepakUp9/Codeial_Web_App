2beddd0dab1a28bbb1d70d9b92a8e3bc02e4c4a5
deepak <deepak8588933265.gmail.com>
2021-04-17 13:19:58 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,8 @@
+module.exports.home=function(req,res){
+
+    return res.end('<h1>Express is up for Codeial!</h1>');
+}
+
+//adding more controller
+//module.exports.actionName=funtion(req,res){}
+
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app=express();
 
 const port=8000;
 
+
+//use express router
+app.use('/',require('./routes'))
+
 app.listen(port,function(err){
 
     if(err){
